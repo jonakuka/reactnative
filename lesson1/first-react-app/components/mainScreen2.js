c59b6296d7730bf13522332f5aacc96d844c5215
jonakuka <jonakuka123@gmail.com>
2025-11-06 20:45:16 +0100

    day 2

---
+++ lesson1/first-react-app/components/mainScreen2.js
@@ -0,0 +1,25 @@
+import React from "react";
+import {Text, StyleSheet, View} from "react-native";
+
+
+const MainScreen=()=>{
+    return(
+        <View style={}>
+            <Text style={styles.textStyle}>This is main screen 2</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+
+    container={
+        flex=1;
+        backgroundcolor: "#9c4c4cff" ,
+        alignItems: "center"
+    }
+    textStyle:{
+        fontSize: 30,
+    },
+})
+
+export default MainScreen2
